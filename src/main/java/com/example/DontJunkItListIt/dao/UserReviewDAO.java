353c2ce008610dcfdbdd7970aa5859ffe204eacb
SumanthDande <SDande@clarku.edu>
2023-11-09 18:00:35 -0500

    Created and Implemented UserReviewDAO

---
+++ src/main/java/com/example/DontJunkItListIt/dao/UserReviewDAO.java
@@ -0,0 +1,12 @@
+package com.example.DontJunkItListIt.dao;
+
+import java.util.List;
+
+
+import com.example.DontJunkItListIt.entity.CarParts;
+import com.example.DontJunkItListIt.entity.UserReview;
+
+public interface UserReviewDAO {
+    List<UserReview> findByReviewedPart(CarParts reviewedPart);
+}
+
